d4d033aee65acdda18b32d3b2982b53ea6366249
Aditya <aditya2012058@akgec.ac.in>
2024-12-19 20:08:40 +0530

    implement routing with react-router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import React from "react";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Genres from "./pages/Genres";
+import BestSellers from "./pages/BestSellers";
+import TopAuthors from "./pages/TopAuthors";
+import Wishlist from "./pages/Wishlist";
+import Cart from "./pages/Cart";
+import LogIn from "./pages/Login";
+import "./index.css";
+import "./app.css";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/genres" element={<Genres />} />
+        <Route path="/best-sellers" element={<BestSellers />} />
+        <Route path="/top-authors" element={<TopAuthors />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 };
 
