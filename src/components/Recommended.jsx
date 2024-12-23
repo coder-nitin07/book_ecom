6be6fb63cffd32b52627ec199b5c608c3bc88f62
Aditya <aditya2012058@akgec.ac.in>
2024-12-24 00:51:04 +0530

    Refactor Recommended component to use dynamic book data and improve layout

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
+const books = [
+  {
+    title: "Harry Potter",
+    author: "JK Rowling",
+    image:
+      "https://www.madrasshoppe.com/207337-large_default/harry-potter-7-volume-children-s-paperback-boxed-set-the-complete-collection-set-of-7-volumes-jk-rowling.jpg",
+  },
+  {
+    title: "Harry Potter",
+    author: "JK Rowling",
+    image:
+      "https://www.madrasshoppe.com/207337-large_default/harry-potter-7-volume-children-s-paperback-boxed-set-the-complete-collection-set-of-7-volumes-jk-rowling.jpg",
+  },
+  {
+    title: "Harry Potter",
+    author: "JK Rowling",
+    image:
+      "https://www.madrasshoppe.com/207337-large_default/harry-potter-7-volume-children-s-paperback-boxed-set-the-complete-collection-set-of-7-volumes-jk-rowling.jpg",
+  },
+  {
+    title: "Harry Potter",
+    author: "JK Rowling",
+    image:
+      "https://www.madrasshoppe.com/207337-large_default/harry-potter-7-volume-children-s-paperback-boxed-set-the-complete-collection-set-of-7-volumes-jk-rowling.jpg",
+  },
+];
+
 const Recommended = () => {
   return (
     <div className="mx-4 md:mx-14 my-10 md:my-16">
@@ -9,60 +36,31 @@ const Recommended = () => {
         </h1>
 
         {/* CARD CONTAINER */}
-        <div className="flex flex-col md:flex-row justify-center gap-10 md:gap-16 text-black mx-6 md:mx-20 my-4 md:my-8">
-          <div className="bg-white rounded-xl w-full md:w-1/3">
-            <div className="m-4">
-              <img
-                src="https://www.madrasshoppe.com/207337-large_default/harry-potter-7-volume-children-s-paperback-boxed-set-the-complete-collection-set-of-7-volumes-jk-rowling.jpg"
-                alt="Harry Potter Books"
-                className="w-full h-auto"
-              />
-            </div>
-            <div className="bg-zinc-400 rounded-b-xl">
-              <div className="p-4">
-                <h1 className="text-xl md:text-2xl font-semibold">
-                  Harry Potter
-                </h1>
-                <p className="font-semibold text-zinc-700">By JK Rowling</p>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8 text-black mx-2 md:mx-20 my-4 md:my-8">
+          {books.map((book) => (
+            <div
+              key={book.title}
+              className="bg-white rounded-xl transform transition-transform duration-300 hover:scale-105"
+            >
+              <div className="m-4">
+                <img
+                  src={book.image}
+                  alt={book.title}
+                  className="w-full h-auto"
+                />
+              </div>
+              <div className="bg-zinc-400 rounded-b-xl">
+                <div className="p-4">
+                  <h1 className="text-lg md:text-xl font-semibold">
+                    {book.title}
+                  </h1>
+                  <p className="text-sm md:text-base font-semibold text-zinc-700">
+                    {book.author}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="bg-white rounded-xl w-full md:w-1/3">
-            <div className="m-4">
-              <img
-                src="https://www.madrasshoppe.com/207337-large_default/harry-potter-7-volume-children-s-paperback-boxed-set-the-complete-collection-set-of-7-volumes-jk-rowling.jpg"
-                alt="Harry Potter Books"
-                className="w-full h-auto"
-              />
-            </div>
-            <div className="bg-zinc-400 rounded-b-xl">
-              <div className="p-4">
-                <h1 className="text-xl md:text-2xl font-semibold">
-                  Harry Potter
-                </h1>
-                <p className="font-semibold text-zinc-700">By JK Rowling</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="bg-white rounded-xl w-full md:w-1/3">
-            <div className="m-4">
-              <img
-                src="https://www.madrasshoppe.com/207337-large_default/harry-potter-7-volume-children-s-paperback-boxed-set-the-complete-collection-set-of-7-volumes-jk-rowling.jpg"
-                alt="Harry Potter Books"
-                className="w-full h-auto"
-              />
-            </div>
-            <div className="bg-zinc-400 rounded-b-xl">
-              <div className="p-4">
-                <h1 className="text-xl md:text-2xl font-semibold">
-                  Harry Potter
-                </h1>
-                <p className="font-semibold text-zinc-700">By JK Rowling</p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
